4ce9c4569e7b0471dae144911953a570f21c6dcb
xcplaza <xcplaza@gmail.com>
2024-04-03 15:06:08 +0300

    update

--- CheckLogID.ps1
+++ CheckLogID.ps1
@@ -1,13 +1,20 @@
-#CHECK LOGs ID WINDOWS 
+$Servers = Get-ADComputer -Filter * -Properties * | Where-Object {$_.OperatingSystem -like '*Windows*Server*' -or $_.Name -match 'TIC-MASAV'} | Select-Object -ExpandProperty Name
 
-$S = Get-ADComputer -Filter * -properties *  | Where-Object {$_.OperatingSystem -like '*Windows*Server*' -or $_.name -match 'TIC-MASAV'} | select Name
+$StartDate = (Get-Date).AddDays(-14)
 
-$sDate = (get-date).AddDays(-1)
-ForEach ($Server in $S) {
-try {Get-WinEvent -FilterHashtable @{logname="System"; id=@(4740, 4719, 4099, 4688, 4670, 4672, 1125, 1006); StartTime=$sDate} -ComputerName $Server.Name -ErrorAction Stop | 
-      Select-Object LogMode, MaximumSizeInBytes, RecordCount, LogName,
-          @{name='ComputerName'; expression={$Server}} | Select ComputerName |  Format-Table -AutoSize}
-    catch [Exception] {
-if ($_.Exception -match "No events were found that match the specified selection criteria") {Write-Host $Server.Name "- No events found";}
+ForEach ($ServerName in $Servers) {
+    try {
+        Get-WinEvent -FilterHashtable @{
+            LogName = "System"
+            ID = @(4740, 4719, 4099, 4688, 4670, 4672, 1125, 1006)
+            StartTime = $StartDate
+        } -ComputerName $ServerName -ErrorAction Stop | 
+        Select-Object @{Name='ServerName'; Expression={$ServerName}}, ID, TimeCreated | 
+        Format-Table -AutoSize
     }
- }
+    catch [Exception] {
+        if ($_.Exception -match "No events were found that match the specified selection criteria") {
+            Write-Host "$ServerName - No events found"
+        }
+    }
+}
