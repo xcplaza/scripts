d86b21b27763688c4009533b9b2c0335db209079
xcplaza <xcplaza@gmail.com>
2024-04-03 11:21:45 +0300

    update

--- GetServerInDomain.ps1
+++ GetServerInDomain.ps1
@@ -1,3 +1,3 @@
 #GET ALL SERVER IN DOMAIN
 
-Get-ADComputer -Filter * -properties * | select Name, Enabled,ipv4address, OperatingSystem
+Get-ADComputer -Filter * -properties *  | Where-Object {$_.OperatingSystem -like '*Windows*Server*'} | select Name, Enabled,ipv4address, OperatingSystem
