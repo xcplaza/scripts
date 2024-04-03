0a351f72d370a406b3d0c02ce5612413050cffdc
xcplaza <xcplaza@gmail.com>
2024-04-03 16:46:16 +0300

    script SendEmail

---
+++ Outlook/SendEmail.ps1
@@ -0,0 +1,12 @@
+#SEND EMAIL
+
+$EmailMessage = @{
+    To         = "exoip@gmail.com"
+    From       = "scanner@exoip.com"
+    Subject    = "Test email"
+    Body       = "Test email sent using Office 365 SMTP relay"
+    SmtpServer = "exoip-com.mail.protection.outlook.com"
+    Port       = "25"
+}
+
+Send-MailMessage @EmailMessage
