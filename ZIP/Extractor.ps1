00ae61178b283e0d20039c6c422187270703673c
xcplaza <xcplaza@gmail.com>
2024-04-09 14:56:00 +0300

    script for extract ARJ files

---
+++ ZIP/Extractor.ps1
@@ -0,0 +1,25 @@
+﻿# Specify the full path to the UnRAR executable
+$UnrarPath = ".\Arj64.exe"
+
+# Check if UnRAR executable exists
+if (-not (Test-Path $UnrarPath)) {
+    Write-Host "UnRAR executable not found. Please make sure UnRAR is installed at '$UnrarPath' or specify the correct path."
+    Exit
+}
+
+# Specify the ARJ file path
+$ArjFilePath = "*.ARJ"
+
+# Check if the ARJ file exists
+if (-not (Test-Path $ArjFilePath)) {
+    Write-Host "ARJ file not found at $ArjFilePath."
+    Exit
+}
+
+# Execute UnRAR to extract ARJ file
+try {
+    & $UnrarPath x "$ArjFilePath" -y
+    Write-Host "Extraction successful."
+} catch {
+    Write-Host "Error occurred while extracting ARJ archive: $_"
+}
---
+++ ZIP/Extractor7.ps1
@@ -0,0 +1,28 @@
+﻿# Specify the full path to the 7-Zip executable
+$7ZipPath = ".\7z.exe"
+
+# Check if 7-Zip executable exists
+if (-not (Test-Path $7ZipPath)) {
+    Write-Host "7-Zip executable not found. Please make sure 7-Zip is installed at '$7ZipPath' or specify the correct path."
+    Exit
+}
+
+# Specify the ARJ file path
+$ArjFilePath = "*.ARJ"
+
+# Check if the ARJ file exists
+if (-not (Test-Path $ArjFilePath)) {
+    Write-Host "ARJ file not found at $ArjFilePath."
+    Exit
+}
+
+# Specify the output directory
+$OutputDirectory = ".\"
+
+# Execute 7-Zip to extract ARJ file
+try {
+    & $7ZipPath x "$ArjFilePath" -o"$OutputDirectory" -aoa
+    Write-Host "Extraction successful."
+} catch {
+    Write-Host "Error occurred while extracting ARJ archive: $_"
+}
