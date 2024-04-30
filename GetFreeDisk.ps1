19727ac25e6106d9ba87f984319222cea43ab58d
xcplaza <xcplaza@gmail.com>
2024-04-30 15:58:47 +0300

    Update GetFreeDisk.ps1

--- GetFreeDisk.ps1
+++ GetFreeDisk.ps1
@@ -7,12 +7,11 @@ $creds = "eilot.org.il\administrator"
 $servers = @("192.168.100.5", "192.168.100.7")
 Foreach ($server in $servers)
 {
-    $disks = Get-WmiObject Win32_LogicalDisk -ComputerName $server -Filter DriveType=3 | 
+    $disks = Get-WmiObject Win32_LogicalDisk -ComputerName $server -Filter DriveType=3 -credential $creds | 
         Select-Object DeviceID, 
             @{'Name'='Size'; 'Expression'={[math]::truncate($_.size / 1GB)}}, 
             @{'Name'='Freespace'; 'Expression'={[math]::truncate($_.freespace / 1GB)}}
-            -credential $creds
-
+            
 Write-host $server -foregroundcolor green
     #$server
     foreach ($disk in $disks)
