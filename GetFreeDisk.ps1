a4739d33920a80f30df2cbdd3c6efd2e93ebfe22
xcplaza <xcplaza@gmail.com>
2024-04-30 16:27:40 +0300

    Update GetFreeDisk.ps1

--- GetFreeDisk.ps1
+++ GetFreeDisk.ps1
@@ -3,7 +3,10 @@ clear-host
 Write-host "Check Disk..." -foregroundcolor yellow
 Write-host ""
 
-$creds = "eilot.org.il\administrator"
+$username = "eilot.org.il\administrator"
+$password = ConvertTo-SecureString "imp@ctIT2006" -AsPlainText -Force
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList($username, $password)
+
 $servers = @("192.168.100.5", "192.168.100.7")
 Foreach ($server in $servers)
 {
