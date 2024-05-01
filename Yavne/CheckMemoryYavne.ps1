d01807767957b67580f8645e3d9b0e35f7a653e0
xcplaza <xcplaza@gmail.com>
2024-05-01 14:23:12 +0300

    Create CheckMemoryYavne.ps1

---
+++ Yavne/CheckMemoryYavne.ps1
@@ -0,0 +1,25 @@
+﻿#GET MEMORY
+clear-host
+Write-host "Check Memory..." -foregroundcolor yellow
+Write-host ""
+
+$username = "YAVNED.MUNI\administrator2"
+$password = ConvertTo-SecureString "edr!23@4" -AsPlainText -Force
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList($username, $password)
+
+$servers = @("10.50.50.157", "10.50.50.13", "10.50.50.7", "10.50.50.8", "10.50.50.4", "10.50.50.25", "10.50.50.105", "10.50.50.16", "10.50.50.11", "10.50.50.17", "10.50.50.15", "10.50.50.19", "10.50.50.12", "10.50.50.18", "10.50.50.32", "10.50.50.60", "10.50.50.24")
+
+foreach ($server in $servers) {
+    try {
+        $memoryUsage = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $server -Credential $creds |
+            ForEach-Object { [math]::Round(($_.TotalVisibleMemorySize - $_.FreePhysicalMemory) / $_.TotalVisibleMemorySize * 100, 2) }
+        
+        if ($memoryUsage -gt 85) {
+            Write-Host "$($server) - $($memoryUsage)%" -ForegroundColor Red
+        } else {
+            Write-Host "$($server) - $($memoryUsage)%" -ForegroundColor Green
+        }
+    } catch {
+        Write-Host "Error retrieving memory usage for $($server): $_" -ForegroundColor Yellow
+    }
+}
