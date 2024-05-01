ad40e71715f040cb45bdb955dfa8e9d6c449e75d
xcplaza <xcplaza@gmail.com>
2024-05-01 09:04:43 +0300

    Update GetFreeDisk.ps1

--- GetFreeDisk.ps1
+++ GetFreeDisk.ps1
@@ -3,11 +3,11 @@ clear-host
 Write-host "Check Disk..." -foregroundcolor yellow
 Write-host ""
 
-$username = "eilot.org.il\administrator"
-$password = ConvertTo-SecureString "imp@ctIT2006" -AsPlainText -Force
+$username = "YAVNED.MUNI\administrator2"
+$password = ConvertTo-SecureString "edr!23@4" -AsPlainText -Force
 $creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList($username, $password)
 
-$servers = @("192.168.100.5", "192.168.100.7")
+$servers = @("10.50.50.157", "10.50.50.13", "10.50.50.7", "10.50.50.8", "10.50.50.4", "10.50.50.25", "10.50.50.105", "10.50.50.16", "10.50.50.11", "10.50.50.17", "10.50.50.15", "10.50.50.19", "10.50.50.12", "10.50.50.18", "10.50.50.32", "10.50.50.60", "10.50.50.24")
 Foreach ($server in $servers)
 {
     $disks = Get-WmiObject Win32_LogicalDisk -ComputerName $server -Filter DriveType=3 -credential $creds | 
@@ -20,10 +20,10 @@ Write-host $server -foregroundcolor green
     foreach ($disk in $disks)
     {
         $disks = $disk.DeviceID + $disk.FreeSpace.ToString("N0") + " GB / " + $disk.Size.ToString("N0") + " GB"
-        if (($disk.Freespace / $disk.size)*100 -gt 90) {
-        Write-Host "$disks"
-    } else {
+        if ((($disk.size - $disk.Freespace)/($disk.Freespace + $disk.size)/2) * 100 -gt 90) {
         Write-Host "$disks" -ForegroundColor Red
+    } else {
+        Write-Host "$disks"
     }
 
      }
