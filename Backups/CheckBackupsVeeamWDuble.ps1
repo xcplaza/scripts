6e830ab6ae7f1c64bb3f50d3cbfcdd7e2ef5f4e6
xcplaza <xcplaza@gmail.com>
2024-05-02 15:59:08 +0300

    Create CheckBackupsVeeamWDuble.ps1

---
+++ Backups/CheckBackupsVeeamWDuble.ps1
@@ -0,0 +1,6 @@
+﻿#ALL BACKUPS VEEAM DISC AND WITHOUT DUBLE
+Get-VBRBackupSession | Sort-Object JobName, CreationTime -Descending | 
+    Group-Object JobName | 
+    ForEach-Object { $_.Group[0] } |
+    Select-Object -Unique JobName, JobType, CreationTime, EndTime, Result, State, @{Name="BackupSize";Expression={$_.BackupStats.BackupSize}} | 
+    Format-Table
