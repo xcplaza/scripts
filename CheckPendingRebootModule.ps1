c23fdc4799df537f16923dbf86f8d96cadeea62e
xcplaza <xcplaza@gmail.com>
2024-05-02 15:59:30 +0300

    Delete CheckPendingRebootModule.ps1

--- CheckPendingRebootModule.ps1
+++
@@ -1,4 +0,0 @@
-#CHECKING PENDING REBOOT
-
-$server = Get-ADComputer -Filter * | Where-Object {$_.Name -like "TIC-*"} | Select -Property Name
-Test-PendingReboot -ComputerName $server.Name -Detailed -SkipConfigurationManagerClientCheck | select ComputerName, IsRebootPending
