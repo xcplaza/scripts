ac53b446d49fb801fe7767a6d1c423ff433ced76
xcplaza <xcplaza@gmail.com>
2024-05-12 16:28:21 +0300

    Update CheckFreeDiskYavne.ps1

--- Yavne/CheckFreeDiskYavne.ps1
+++ Yavne/CheckFreeDiskYavne.ps1
@@ -3,8 +3,8 @@ clear-host
 Write-host "Check Disk Yavne..." -foregroundcolor yellow
 Write-host ""
 
-$username = "YAVNED.MUNI\administrator2"
-$password = ConvertTo-SecureString "edr!23@4" -AsPlainText -Force
+$username = "DOMAIN\administrator"
+$password = ConvertTo-SecureString "*******" -AsPlainText -Force
 $creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList($username, $password)
 
 $servers = @("10.50.50.157", "10.50.50.13", "10.50.50.7", "10.50.50.8", "10.50.50.4", "10.50.50.25", "10.50.50.105", "10.50.50.16", "10.50.50.11", "10.50.50.17", "10.50.50.15", "10.50.50.19", "10.50.50.12", "10.50.50.18", "10.50.50.32", "10.50.50.60", "10.50.50.24")
