a185331426589ce4a5fb05d539f29358fe16d253
xcplaza <xcplaza@gmail.com>
2024-05-13 09:19:31 +0300

    Create GetVMsWithPath.ps1

---
+++ VmWare/GetVMsWithPath.ps1
@@ -0,0 +1,26 @@
+﻿#GET VMs Vmware with path
+clear-host
+Write-host "Connect to Vmware..." -foregroundcolor yellow
+Write-host ""
+
+# Подключаем модуль PowerCLI для работы с VMware
+Import-Module VMware.PowerCLI
+
+# Подключаемся к vCenter Server или ESXi-хосту
+Connect-VIServer 10.50.50.50
+Write-host ""
+
+# Получаем список виртуальных машин
+$vms = Get-VM
+
+# Выводим имя виртуальной машины и путь к её диску
+foreach ($vm in $vms) {
+    $vmName = $vm.Name
+    $vmDiskPath = $vm.ExtensionData.Config.Hardware.Device | Where-Object {$_.GetType().Name -eq "VirtualDisk"} | Select-Object -First 1 | Select-Object -ExpandProperty Backing | Select-Object -ExpandProperty FileName
+    Write-Host "VM: $vmName"
+    Write-Host "Path: $vmDiskPath"
+    Write-Host "-----------------------------------------------------------"
+}
+
+# Отключаемся от vCenter Server или ESXi-хоста
+#Disconnect-VIServer -Confirm:$false
