65692ab6a4e27df105ea32129372e478e1362517
xcplaza <xcplaza@gmail.com>
2024-05-23 11:49:40 +0300

    Create getLicenseKey.ps1

---
+++ getLicenseKey.ps1
@@ -0,0 +1,12 @@
+﻿#Get license key
+Clear-Host
+Write-Host "Check license key..." -ForegroundColor Yellow
+Write-Host ""
+
+$service = get-wmiObject -query 'select * from SoftwareLicensingService'
+if($key = $service.OA3xOriginalProductKey){
+	Write-Host 'Product Key:' $service.OA3xOriginalProductKey -ForegroundColor Green
+	$service.InstallProductKey($key)
+}else{
+	Write-Host 'Key not found.' -ForegroundColor red
+}
