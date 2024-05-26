3dfcc61d37d6d6514770f70578b045054c2ae952
xcplaza <xcplaza@gmail.com>
2024-05-26 16:43:11 +0300

    Update CheckBackupsYavne.ps1

--- Yavne/CheckBackupsYavne.ps1
+++ Yavne/CheckBackupsYavne.ps1
@@ -6,9 +6,9 @@ Write-Host "Check Backups..." -ForegroundColor Yellow
 Write-Host ""
 
 # Указываем домен и учетные данные
-$domain = "DOMAIN"
+$domain = "domain"
 $username = "$domain\administrator"
-$password = ConvertTo-SecureString "***" -AsPlainText -Force
+$password = ConvertTo-SecureString "****" -AsPlainText -Force
 $creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
 
 # Список серверов
@@ -29,7 +29,7 @@ $script = {
         $lastSession = $ListSession | where {$_.JobId -eq $job.Id} | sort -Property EndTime | select -Last 1
         $summary += $job | select Name, @{n='LastResult';e={$lastSession.Result}}
         }
-    $summary
+    return $summary | Format-Table -AutoSize
 }
 
 # Выполнение команды на удаленном сервере
