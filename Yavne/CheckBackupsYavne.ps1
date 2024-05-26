d1c4eb35a54be841363a44692aeaf24614fc93f3
xcplaza <xcplaza@gmail.com>
2024-05-26 16:26:38 +0300

    Update CheckBackupsYavne.ps1

--- Yavne/CheckBackupsYavne.ps1
+++ Yavne/CheckBackupsYavne.ps1
@@ -6,22 +6,30 @@ Write-Host "Check Backups..." -ForegroundColor Yellow
 Write-Host ""
 
 # Указываем домен и учетные данные
-$domain = "***"
+$domain = "DOMAIN"
 $username = "$domain\administrator"
 $password = ConvertTo-SecureString "***" -AsPlainText -Force
 $creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
 
 # Список серверов
-$servers = "YavneVeeam"
+$servers = "YavneVeeamSV"
 $serversWithDomain = $servers | ForEach-Object { "$_.$domain" }
 
 # Скрипт для выполнения на удаленном сервере
 $script = {
-    Get-VBRBackupSession | Where-Object { $_.CreationTime -ge (Get-Date).AddDays(-1) } | 
-    Select-Object JobName, JobType, CreationTime, EndTime, Result, State, 
-    @{Name="BackupSize"; Expression = { $_.BackupStats.BackupSize }} | 
-    Sort-Object CreationTime | 
-    Format-Table -AutoSize
+    #Get-VBRBackupSession | ?{$_.JobType -eq "Backup"} | Where-Object { $_.CreationTime -ge (Get-Date).AddDays(-1) } | 
+    #Select-Object JobName, JobType, CreationTime, EndTime, Result, State, 
+    #@{Name="BackupSize"; Expression = { $_.BackupStats.BackupSize}} |
+    #Sort-Object CreationTime | 
+    #Format-Table -AutoSize
+    $ListJobs = Get-VBRJob | ?{$_.JobType -eq "Backup"} | Sort-Object typetostring, name
+    $ListSession = [Veeam.Backup.Core.CBackupSession]::GetAll()
+    $summary = @()
+    foreach ($job in $ListJobs) {
+        $lastSession = $ListSession | where {$_.JobId -eq $job.Id} | sort -Property EndTime | select -Last 1
+        $summary += $job | select Name, @{n='LastResult';e={$lastSession.Result}}
+        }
+    $summary
 }
 
 # Выполнение команды на удаленном сервере
