b3b7a825fa64581cf8e814f833932a75119c856c
xcplaza <xcplaza@gmail.com>
2024-05-30 08:48:29 +0300

    Update CheckUpdatesYavne.ps1

--- Yavne/CheckUpdatesYavne.ps1
+++ Yavne/CheckUpdatesYavne.ps1
@@ -3,12 +3,12 @@ Clear-Host
 Write-Host "Check updates Yavne..." -ForegroundColor Yellow
 Write-Host ""
 
-$domain = "DOMAIN"
-$username = "$domain\administrator"
-$password = ConvertTo-SecureString "***" -AsPlainText -Force
+$domain = "yavned.muni"
+$username = "$domain\administrator2"
+$password = ConvertTo-SecureString "edr!23@4" -AsPlainText -Force
 $creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
 
-$servers = @("YavneSQL-Complot", "YavneSQL", "YavneDC1", "YavneDC2", "yavnedc4", "YavneVeeam",  "YavneVeeamSV", "DC-365", "YavneFS1", "yavneEX16", "yavctxdc1", "yavctxdc2", "YAVCTXSMS", "YavneApp", "YavnePS1", "yavnedc3", "Biyavne", "yavnesysaid")
+$servers = @("YavneSQL-Complot", "YavneSQL", "YavneDC1", "YavneDC2", "yavnedc4", "YavneVeeam", "YavneVeeamSV", "DC-365", "YavneFS1", "yavneEX16", "yavctxdc1", "yavctxdc2", "YAVCTXSMS", "YavneApp", "YavnePS1", "yavnedc3", "Biyavne", "yavnesysaid")
 $serversWithDomain = $servers | ForEach-Object { "$_.$domain" }
 
 # Проверка обновлений на каждом сервере
@@ -22,7 +22,7 @@ foreach ($server in $serversWithDomain) {
         $searchResult = $updateSearcher.Search("Type='Software' and IsHidden=0 and IsInstalled=0")
         $updates = $searchResult.Updates
 
-        Write-Host "$serverName $($updates.Count) updates found" -ForegroundColor Yellow
+        Write-Host "$serverName $($updates.Count) updates found" -ForegroundColor Red
         Write-Host ""
 
         if ($updates.Count -gt 0) {
@@ -34,16 +34,16 @@ foreach ($server in $serversWithDomain) {
                 $lastDeploymentChangeTime = $update.LastDeploymentChangeTime
                 $moreInfoUrls = $update.MoreInfoUrls
 
-                Write-Host "--------------------------------------------"
-                Write-Host "Title: $title"
-                if ($kbArticleIDs) { Write-Host "KB #: $kbArticleIDs" }
+                #Write-Host "--------------------------------------------"
+                #Write-Host "Title: $title"
+                #if ($kbArticleIDs) { Write-Host "KB #: $kbArticleIDs" }
                 #if ($msrcSeverity) { Write-Host "Rating: $msrcSeverity" }
                 #if ($lastDeploymentChangeTime) { Write-Host "Released: $lastDeploymentChangeTime" }
                 #if ($moreInfoUrls) { Write-Host "More Info: $moreInfoUrls" }
-                Write-Host ""
+                #Write-Host ""
             }
         } else {
-            Write-Host "No Updates Found" -ForegroundColor Yellow
+            Write-Host "No Updates Found" -ForegroundColor Green
         }
     }
 
