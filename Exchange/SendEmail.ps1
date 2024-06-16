6f5489b1fd4b3111babe8d2fca0430d102d02c2d
xcplaza <xcplaza@gmail.com>
2024-06-16 12:54:13 +0300

    Create SendEmail.ps1

---
+++ Exchange/SendEmail.ps1
@@ -0,0 +1,2 @@
+#send email from exchange shell
+Send-MailMessage -SmtpServer YavneEX16.yavned.muni -Port 25 -From Administrator@yavne.muni.il -To elchin@impact-networks.co.il -Subject test -Body test
