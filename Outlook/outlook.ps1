1efac31aa01617222743d44d2a0048d088e85a0a
xcplaza <xcplaza@gmail.com>
2024-06-18 13:07:49 +0300

    Update outlook.ps1

--- Outlook/outlook.ps1
+++ Outlook/outlook.ps1
@@ -4,6 +4,10 @@ Install-Module MSOnline -Force
 Install-Module AzureAD -Force
 Install-Module AzureADPreview -Force
 
+#install module for to connect to Microsoft Graph PowerShell
+Install-Module Microsoft.Graph -Force
+Install-Module Microsoft.Graph.Beta -AllowClobber -Force
+
 #connect to MsolUser
 Connect-MsolService
 
@@ -17,13 +21,13 @@ Get-MsolUser -ReturnDeletedUsers | FL UserPrincipalName,immutableID
 #connect to O365
 Install-Module -Name ExchangeOnlineManagement
 Connect-ExchangeOnline
-Connect-ExchangeOnline -UserPrincipalName hsaidian@keystonedental.com
+Connect-ExchangeOnline -UserPrincipalName impact@gfnadlan.com
 
 #close connection
 [Microsoft.Online.Administration.Automation.ConnectMsolService]::ClearUserSessionState()
 
 #get message
-Get-MessageTrace -RecipientAddress "pds@keystonedental.com" -StartDate "2024-03-19" -EndDate "2024-03-24" | Format-Table -Property Received, SenderAddress, Subject
+Get-MessageTrace -RecipientAddress "eyal@gfnadlan.com" -StartDate "2024-06-16" -EndDate "2024-06-18" | Format-Table -Property Received, SenderAddress, Subject
 
 
 Test-NetConnection -Port 444 -ComputerName 185.145.254.249
