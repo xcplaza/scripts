9e6c120f2a06a65c4efe046a9c7cb23f1494a2c4
xcplaza <xcplaza@gmail.com>
2024-06-30 09:09:04 +0300

    Create RemoteReboot.ps1

---
+++ CheckPC/RemoteReboot.ps1
@@ -0,0 +1,8 @@
+﻿Invoke-Command -HostName IP_ADDRESS -UserName administrator -ScriptBlock {Restart-Computer}
+Sleep 30  ## wait before checking
+$SSH=$null
+While(-not $SSH){
+  Try  {$SSH = New-PSSession -HostName 'MyServer' -ea Stop}
+  Catch{Write-Host 'Waiting for reboot...'}
+  sleep 10
+}
