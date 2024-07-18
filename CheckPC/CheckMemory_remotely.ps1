d72f8b9c7ab1da9af5953d670a0ed8557af963c6
xcplaza <xcplaza@gmail.com>
2024-07-18 10:05:35 +0300

    Create CheckMemory_remotely.ps1

---
+++ CheckPC/CheckMemory_remotely.ps1
@@ -0,0 +1,30 @@
+﻿#GET MEMORY
+clear-host
+$currentTime = Get-Date -format "dd-MMM-yyyy HH:mm:ss"
+Write-host "Check Memory Yavne - $currentTime" -foregroundcolor yellow
+Write-host ""
+
+$domain = "******"
+$username = "$domain\******"
+$password = ConvertTo-SecureString "******" -AsPlainText -Force
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList($username, $password)
+
+$servers = @("******", "******")
+$serversWithDomain = $servers | ForEach-Object { "$_.$domain" }
+
+foreach ($server in $serversWithDomain) {
+    try {
+        $memoryUsage = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $server -Credential $creds |
+        ForEach-Object { [math]::Round(($_.TotalVisibleMemorySize - $_.FreePhysicalMemory) / $_.TotalVisibleMemorySize * 100, 2) }
+        
+        if ($memoryUsage -gt 85) {
+            Write-Host "$($server) - $($memoryUsage)%" -ForegroundColor Red
+        }
+        else {
+            Write-Host "$($server) - $($memoryUsage)%" -ForegroundColor Green
+        }
+    }
+    catch {
+        Write-Host "Error retrieving memory usage for $($server): $_" -ForegroundColor Yellow
+    }
+}
